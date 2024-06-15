5bf03ed318498c8e8b8626fe6a1addd5874006bf
camila <129806557+arosteguicp@users.noreply.github.com>
2024-06-14 19:07:26 -0500

    Update spimi_prepro.py
    diccionario de words funcional

--- spimi_prepro.py
+++ spimi_prepro.py
@@ -25,7 +25,7 @@ os.makedirs(output_folder, exist_ok=True)
 stoplist = ["a", "an", "and", "are", "as", "at", "be", "but", "by",
             "for", "if", "in", "into", "is", "it", "no", "not", "of",
             "on", "or", "such", "that", "the", "their", "then", "there",
-            "these", "they", "this", "to", "was", "will", "with"]
+            "these", "they", "this", "to", "was", "will", "with", "&", ",", "."]
 
 for index, row in df.iterrows():
     # Concatenar todos los atributos en una sola cadena
