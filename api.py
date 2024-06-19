1393767081a59272798d8176bcddbac3548a753a
bihoepiro <83984593+bihoepiro@users.noreply.github.com>
2024-06-18 23:54:30 -0500

    Add files via upload

---
+++ api.py
@@ -0,0 +1,68 @@
+# app.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import psycopg2
+from os import environ
+import SPIMIIndex  # Importar el archivo de indexación
+
+app = Flask(__name__)
+CORS(app)
+
+# Cargar los bloques y otras variables globales
+bloques_cargados, cant_docs = SPIMIIndex.cargar_bloques_y_docs()
+
+# Función para conectar con PostgreSQL
+def connect_to_postgres():
+    return psycopg2.connect(
+        host=environ.get('DB_HOST', 'localhost'),
+        database=environ.get('DB_NAME', 'proyecto'),
+        port=environ.get('DB_PORT', '5432'),
+        user=environ.get('DB_USER', 'postgres'),
+        password=environ.get('DB_PASSWORD', '210904')
+    )
+
+# Endpoint para la búsqueda
+@app.route('/search', methods=['POST'])
+def search():
+    data = request.get_json()
+    query = data.get('query')
+    top_k = int(data.get('topK', 10))  # Default de 10 si no se especifica
+    indexing_method = data.get('indexingMethod')
+
+    if not query or not indexing_method:
+        return jsonify(error="Invalid input"), 400
+
+    if indexing_method == 'PostgreSQL':
+        try:
+            # Conectar a PostgreSQL y realizar búsqueda con índice GIN
+            conn = connect_to_postgres()
+            cursor = conn.cursor()
+
+            postgreSQL_select = """
+                SELECT track_name, lyrics, duration_ms, ts_rank(indexed, query) AS rank
+                FROM spotify_songs, plainto_tsquery('multilingual', %s) query
+                ORDER BY rank DESC
+                LIMIT %s;
+            """
+
+            cursor.execute(postgreSQL_select, (query, top_k))
+            results = [{'track_name': row[0], 'lyrics': row[1], 'duration_ms': row[2], 'rank': row[3]} for row in cursor.fetchall()]
+
+            cursor.close()
+            conn.close()
+
+            return jsonify(results=results)
+        except Exception as e:
+            return jsonify(error=str(e)), 500
+    elif indexing_method == 'Índice local':
+        try:
+            # Procesar la consulta utilizando el índice local
+            top_k_documentos = SPIMIIndex.procesar_consulta(query, top_k, bloques_cargados, cant_docs)
+            return jsonify(results=top_k_documentos)
+        except Exception as e:
+            return jsonify(error=str(e)), 500
+    else:
+        return jsonify(error="Invalid indexing method"), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
